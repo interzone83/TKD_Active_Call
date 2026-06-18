7769cd8dd7bc0b6b2006b61acde792bad6c53a5f
interzone83 <charlie.perera@gmail.com>
2026-06-18 07:52:12 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
  * ---------
  * Paste your deployed Apps Script /exec URL here.
  */
-const API_URL = "https://script.google.com/macros/s/AKfycbxMAGExWFM50t-cWEZe6XXCG0N_6UzlFt8OaBCZUY1FedM47aMv567Mh6vaIL71JGA2/exec";
+const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_EXEC_URL_HERE";
