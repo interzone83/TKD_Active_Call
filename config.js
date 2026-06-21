daeae0416b32a844f56b377385569ea2211358dc
interzone83 <charlie.perera@gmail.com>
2026-06-21 22:04:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
  * ---------
  * Paste your deployed Apps Script /exec URL here.
  */
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_EXEC_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbxZsZ50-SwB5fBB1-bYEkWvwHeJ8M0lLZSHwDj8_-w8zZ8aISuU2HlRtv8kLE6HMZEg/exec";
